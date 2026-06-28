f1b3cbf01aa633ae63310d5951de6c542e930f4f
alexpfeffer4 <alexpfeffer4@gmail.com>
2026-06-28 19:33:25 -0400

    Remove runtime CSS file read

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,4 @@
-import fs from "node:fs";
-import path from "node:path";
-
 import "./globals.css";
-
-const globalStyles = fs.readFileSync(path.join(process.cwd(), "app", "globals.css"), "utf8");
 
 export const metadata = {
   title: "Kalshi L Notifier",
@@ -13,9 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
-      </head>
       <body>{children}</body>
     </html>
   );
